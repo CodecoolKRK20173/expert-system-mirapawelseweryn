091c2ab536ff04497a282ac2daffc31e0d2af7ea
Seweryn91 <34771956+Seweryn91@users.noreply.github.com>
2018-05-30 12:14:41 +0200

    Add parseFact method

--- src/com/codecool/ExpertSystem/FactParser.java
+++ src/com/codecool/ExpertSystem/FactParser.java
@@ -2,9 +2,11 @@
 
 import com.sun.org.apache.xerces.internal.parsers.XMLParser;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import com.codecool.ExpertSystem.*;
+import java.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import java.io.*;
 
 public class FactParser extends XMLParser{
     private FactRepository factRepository;
@@ -17,5 +19,35 @@ public class FactParser extends XMLParser{
 
     public FactRepository getFactRepository(){
         return this.factRepository;
+
+    public void parseFact(){
+        super.document.getDocumentElement().normalize();
+        NodeList factsList = super.document.getElementsByTagName("Fact");
+            for(int i = 0; i< factsList.getLength(); i++){
+                Node nNode = factsList.item(i);
+                if(nNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element eElement = (Element) nNode;
+
+                    String factId = eElement.getAttribute("id");
+                    String factDescription = eElement.getChildNodes()
+                                                    .item(1)
+                                                    .getAttributes()
+                                                    .item(0)
+                                                    .getTextContent();
+
+                    Fact newFact = new Fact(id, description);
+                    NodeList evals = eElement.getElementsByTagName("Eval");
+
+                    for(int j = 0; j < evals.getLength(); j++){
+                        Element eval = (Element) evals.item(j);
+                        String factEvalId = eval.getAttribute("id");
+                        String factEvalValue = eval.getTextContent();
+                        newFact.setFactValueById(factEvalId, Boolean.valueOf(factEvalValue));
+                    }
+                    factRepository.addFact(newFact);
+
+                }
+            }
+
     }
 }
