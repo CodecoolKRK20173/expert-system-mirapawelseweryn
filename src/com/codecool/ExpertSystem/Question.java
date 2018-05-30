0c3de7ba2a28c8a0518cf415ea2fe98217fcf3fa
Paul <bielcu@gmail.com>
2018-05-30 09:39:59 +0200

    implement question class

---
+++ src/com/codecool/ExpertSystem/Question.java
@@ -0,0 +1,27 @@
+public class Question {
+  public String id;
+  public String question;
+  public Answer answer;
+
+  Question(String id, String question, Answer answer) {
+    this.id = id;
+    this.question = question;
+    this.answer = answer;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public Answer getAnswer() {
+    return answer;
+  }
+
+  public boolean getEvaluatedAnswer(String input) {
+    return answer.evaluateAnswerByInput(input);
+  }
+}
