886d4f993e5a519d8c4a2e2b8324ceda548ec56f
Mirosława Bryja <mira.bryja@gmail.com>
2018-05-30 10:23:44 +0200

    Add creating questions from xml file

--- src/com/codecool/ExpertSystem/RuleParser.java
+++ src/com/codecool/ExpertSystem/RuleParser.java
@@ -1,14 +1,35 @@
 import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
-class RuleParser extends XMLParser {
+public class RuleParser extends XMLParser {
     private RuleRepository ruleRepository;
+    private final int INDEX = 0;
     
-    public XMLParser(String fileName){
-        RuleRepository ruleRepository = new RuleRepository();
+    public RuleParser(String fileName){
         super.loadXMLDocument(fileName);
+        RuleRepository ruleRepository = new RuleRepository();
+        
     }
 
     public RuleRepository getRuleRepository() {
         return this.ruleRepository;
     }
+    public void createQuestion() {
+        super.document.getDocumentElement().normalize();
+        NodeList rulesList = super.document.getElementsByTagName("Rule");
+
+        for (int i = 0; i < rulesList.getLength(); i++) {
+		    Node rule = rulesList.item(i);
+            if (rule.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) rule;
+                this.ruleRepository.addQestion(new Question(eElement.getAttribute("id"), 
+                                                            eElement.getElementsByTagName("Question").item(INDEX).getTextContent(),
+                                                            generateAnswer(eElement)));
+            }
+
+        }
+    }
+    
 }
