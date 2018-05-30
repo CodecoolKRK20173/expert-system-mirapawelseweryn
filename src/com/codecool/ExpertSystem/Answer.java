71b61b46d76714265a6e698541744d1bcb7df7e2
Paul <bielcu@gmail.com>
2018-05-30 11:40:04 +0200

    implement Answer class

---
+++ src/com/codecool/ExpertSystem/Answer.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+  private List<Value> values = new ArrayList<>();
+
+  public boolean evaluateAnswerByInput(String input) {
+    List<String> valuesStr = new ArrayList<>();
+    valuesStr = Arrays.asList(input.split(","));
+
+    for (Value value : values) {
+      for (String str : valuesStr) {
+        if (value.getInputPattern().contains(str)) {
+          return value.getSelectionType();
+        }
+      }
+    }
+    return false;
+  }
+
+  public void addValue(Value value) {
+    values.add(value);
+  }
+}
